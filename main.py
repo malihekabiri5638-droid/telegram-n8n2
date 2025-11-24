ba79a83e90ac067a08acf9637e36904b0c11743e
malihekabiri5638-droid <malihe.kabiri5638@gmail.com>
2025-11-24 17:37:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# telegram-n8n2
-import os
-from telethon import TelegramClient, events
-import requests
-api_id = int(os.environ.get('API_ID'))
-api_hash = os.environ.get('API_HASH')
-phone = os.environ.get('PHONE')
 import os
 from telethon import TelegramClient, events
 import requests
